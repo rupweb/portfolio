0a1b9f57d835cc5516cee3aedb7147a254f22770
rupweb <rupert@webstersystems.co.uk>
2024-03-06 17:25:41 +0000

    Added news and styling

--- src/components/Left.js
+++ src/components/Left.js
@@ -3,10 +3,10 @@ import '../styles/left.css';
 
 const Left = () => (
   <div className="two-columns-left">
-    <div className="column left-column">
+    <div className="column left-column-left">
       <img src="/images/left.jpg" alt="Left Image" />
     </div>
-    <div className="column right-column">
+    <div className="column right-column-left">
       <img src="/images/cyber1.jpg" alt="Cyber Image" />
       <div>Row 2</div>
       <img src="/images/cyber2.jpg" alt="Cyber Image" />
--- src/components/Right.js
+++ src/components/Right.js
@@ -1,17 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../styles/right.css';
 
-const Right = () => (
-  <div className="two-columns-right">
-    <div className="column left-column-right">
-      <div>Row 1</div>
-      <div>Row 2</div>
-      <img src="/images/cyber3.jpg" alt="Cyber Image" />
+const Right = () => {
+  const [articles, setArticles] = useState([]);
+  const apiKey = 'e70f3d20bd0a4c33a84b06e3dc660505';
+
+  useEffect(() => {
+    fetch(`https://newsapi.org/v2/top-headlines?category=business&language=en&country=us&apiKey=${apiKey}`)
+      .then(response => response.json())
+      .then(data => {
+        if (data.status === 'ok') {
+          setArticles(data.articles);
+        }
+      })
+      .catch(error => {
+        console.error('Error fetching news', error);
+      });
+  }, []);
+
+  return (
+    <div className="two-columns-right">
+      <div className="column left-column-right">
+        <div>Row 1</div>
+        <div>
+          <ul>
+            {articles.map((article, index) => (
+              <li key={index}>{article.title}</li>
+            ))}
+          </ul>
+        </div>
+        <img src="/images/cyber3.jpg" alt="Cyber Image" />
+      </div>
+      <div className="column right-column-right">
+        <img src="/images/right.jpg" alt="Right Image" />
+      </div>
     </div>
-    <div className="column right-column-right">
-      <img src="/images/right.jpg" alt="Right Image" />
-    </div>
-  </div>
-);
+  );
+};
 
 export default Right;
