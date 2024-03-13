b53d4c3e61e9a9b70e6f4844a5b00281ef400fc4
rupweb <rupert@webstersystems.co.uk>
2024-03-13 16:10:21 +0000

    After tidying up tests

--- jest.setup.js
+++
@@ -1,6 +0,0 @@
-require('@testing-library/jest-dom');
-
-// Polyfills
-const { ResizeObserver } = require('@juggle/resize-observer');
-global.ResizeObserver = ResizeObserver;
-require('whatwg-fetch');
